aa9e5b3b4aeb9defd9ccebf46982f4c5718cf104
oscar <navarroscar@gmail.com>
2020-10-26 00:49:51 -0500

    avance crud unidad medida
    crud unidad de medida-pendiente el editar

---
+++ server/Clases/registroUM.php
@@ -0,0 +1,33 @@
+<?php 	
+	require_once('conexion.php');
+
+	//$idproducto	= $_POST["idproducto"];
+	$unidadMedida		=  strtoupper( $_POST["unidadMedida"]);
+	$descripcionUnidad	= strtoupper($_POST["descripcionUnidad"]);
+	/*
+	$unidadMedida="UN";
+	$descripcionUnidad="UNIDAD";*/
+
+
+	
+
+	$str = "
+		INSERT INTO unidadMedida(
+		unidadMedida, 
+		descripcionUnidad
+		)
+		VALUES(
+		'".$unidadMedida."',
+		'".$descripcionUnidad."');
+	
+	";
+
+	$crearUnidadMedida = mysqli_query($conexion, $str) or die('no se creo la unidad de Medida');
+
+	if($crearUnidadMedida) {
+		echo "0";
+	}else {
+		echo "1";
+	}
+
+?>
