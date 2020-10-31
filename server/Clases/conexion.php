99a4c0e85bd965ba2f03299081f28bb56b1521dd
sebasgarci99 <53800085+sebasgarci99@users.noreply.github.com>
2020-10-30 19:01:16 -0500

    Update conexion.php
    v1.1.3

--- server/Clases/conexion.php
+++ server/Clases/conexion.php
@@ -5,24 +5,8 @@
 	$based = "recaudos_ferreApp";
 	
 	$conexion = mysqli_connect($servidor, $usuario, $clave) or die('No se conecto al servidor');
-<<<<<<< Updated upstream
 
 	mysqli_set_charset( $conexion, 'utf8');
 	
 	mysqli_select_db($conexion, $based) or die('No se encontro la base de datos');
 
-
-/*$str="
-	ALTER TABLE unidadmedida AUTO_INCREMENT = 1
-	";
-
-
-	$resultado = mysqli_query($conexion, $str) or die('no se consulto el stock');*/
-	
-?>
-=======
-	mysqli_set_charset( $conexion, 'utf8');
-	mysqli_select_db($conexion, $based) or die('No se encontro la base de datos');
-
-?>
->>>>>>> Stashed changes
