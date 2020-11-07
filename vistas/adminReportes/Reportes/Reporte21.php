afe224b9b53fe2547ea8d290a8f5366b2ff8b254
sebasgarci99 <53800085+sebasgarci99@users.noreply.github.com>
2020-11-06 20:43:13 -0500

    Botones de descarga para el reporte 21
    v1.1.5

--- vistas/adminReportes/Reportes/Reporte21.php
+++ vistas/adminReportes/Reportes/Reporte21.php
@@ -114,9 +114,16 @@
 	<script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" data-auto-replace-svg="nest"></script>
 	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
 	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-	
 
-
+	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+	<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+	<script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
+	<script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.flash.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+	<script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.html5.min.js"></script>
+	<script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.print.min.js"></script>
 
 
 
@@ -143,7 +150,7 @@
         "url": "../Reportes/Reporte21SQL.php"
 
     	},
-
+    	
 		"columns": [
 			{ "data": "idCategoria" },      
 			{ "data": "categoria" },
